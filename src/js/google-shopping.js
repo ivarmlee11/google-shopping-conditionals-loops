95fa997d6dde639d90637242e49a14b2774fb4d4
Ivar Lee <ivarr5@gmail.com>
2016-05-26 00:01:28 -0700

    linted, ignored error concerning -i used outside of binding context-

--- src/js/google-shopping.js
+++ src/js/google-shopping.js
@@ -3,55 +3,55 @@ var data = require('../products.json');
 // Write your solutions below
 // Exercise 1
 
-// var count = 0;
-
-
-for (var i = 0; i < data.items.length; i++){
-  if(data.items[i].kind === "shopping#product"){
-    count = count + 1;
+var count;
+for (var i = 0; i < data.items.length; i++) {
+  if (data.items[i].kind === 'shopping#product') {
+    count++;
   }
 }
 console.log(count);
 
-//Exercise 2
+// Exercise 2
 
 
-for (var i = 0; i < data.items.length; i++){
-  if(data.items[i].product.inventories[0].availability === "backorder"){
-    console.log('hello');
+for (var i = 0; i < data.items.length; i++) {
+  if (data.items[i].product.inventories[0].availability === 'backorder') {
     console.log(data.items[i].product.title);
   }
 }
 
-//Exercise 3
+// Exercise 3
 
 
-for (var i = 0; i < data.items.length; i++){
-  if(data.items[i].product.images.length > 1){
+for (var i = 0; i < data.items.length; i++) {
+  if (data.items[i].product.images.length > 1) {
     console.log(data.items[i].product.title);
   }
 }
 
 // Exercise 4
 
-for (var i = 0; i < data.items.length; i++){
-  if(data.items[i].product.brand === "Canon"){
+for (var i = 0; i < data.items.length; i++) {
+  if (data.items[i].product.brand === 'Canon') {
     console.log(data.items[i].product.title);
   }
 }
 
 // Exercise 5
 
-for (var i = 0; i < data.items.length; i++){
-  if(data.items[i].product.brand === "Canon" && data.items[i].product.author.name === "eBay"){
+for (var i = 0; i < data.items.length; i++) {
+  if (data.items[i].product.brand === 'Canon' &&
+   data.items[i].product.author.name === 'eBay') {
     console.log(data.items[i].product.title);
   }
 }
 
 // Exercise 6
+// Print all the products with their brand, price, and an image link
 
- // Print all the products with their brand, price, and an image link
-
-for (var i = 0; i < data.items.length; i++){
-    console.log(data.items[i].product.title + " " + data.items[i].product.brand + " " + data.items[i].product.inventories[0].price + " " + data.items[i].product.images[0].link + '\n');
+for (var j = 0; j < data.items.length; j++) {
+  console.log(data.items[j].product.title
+   + ' ' + data.items[i].product.brand + ' '
+   + data.items[j].product.inventories[0].price
+     + ' ' + data.items[j].product.images[0].link + '\n');
 }
